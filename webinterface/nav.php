1bee169c7921ad50393bba9523c637ad6b8567d5
Julian Merkle <juli.merkle@gmail.com>
2018-05-06 20:55:55 +0200

    Remove application icon

--- webinterface/nav.php
+++ webinterface/nav.php
@@ -14,7 +14,6 @@ if((time() - $job_check['last_update']['timestamp']) < 259200 && !isset($_SESSIO
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="version" content="<?PHP echo $currvers; ?>">
-	<link rel="icon" href="../tsicons/rs.png">
 	<title>TS-N.NET Ranksystem</title>
 	<link href="../libs/combined_wi.css?v=<?PHP echo $currvers; ?>" rel="stylesheet">
 	<script src="../libs/combined_wi.js?v=<?PHP echo $currvers; ?>"></script>
